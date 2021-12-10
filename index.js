5e2ec1b609c55bcf150bf0e2f6947d94bb885a3b
Lukas Keel <lrk83@cornell.edu>
2021-12-10 18:03:10 -0500

    successfully posts to KV

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
+const corsHeaders = {
+  "Access-Control-Allow-Origin": "*",
+  "Access-Control-Allow-Methods": "GET,HEAD,POST,OPTIONS",
+  "Access-Control-Max-Age": "86400",
+}
+
 addEventListener('fetch', event => {
   event.respondWith(handleRequest(event.request))
 })
@@ -6,7 +12,30 @@ addEventListener('fetch', event => {
  * @param {Request} request
  */
 async function handleRequest(request) {
-  return new Response('Hello worker!', {
-    headers: { 'content-type': 'text/plain' },
-  })
+
+  if (request.method === 'POST'){
+
+    let body = await request.json();
+
+    let title = body.title;
+    let username = body.username;
+    let content = body.content;
+
+    if (title===undefined || username===undefined || content===undefined){
+      return new Response("please make sure your post includes username, title, and content as JSON")
+    }
+
+    try {
+      let key = username.trim()+Date.now()
+      let result = await INTERVIEW_KV.put(key,JSON.stringify(body))
+      return new Response("success");
+    }
+    catch(err) {
+      return new Response(err)
+    }
+
+    
+  }
+
+ 
 }
