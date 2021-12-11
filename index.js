3181d1adcf515f72091e4a1286be836ae33219e4
Lukas Keel <lrk83@cornell.edu>
2021-12-11 13:05:47 -0500

    fix json weirdness

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ async function handleRequest(request) {
     let content = body.content;
 
     if (title===undefined || username===undefined || content===undefined){
-      return new Response("please make sure your post includes username, title, and content as JSON")
+      return new Response("please make sure your post includes username, title, and content",{status:500})
     }
 
     try {
@@ -22,7 +22,7 @@ async function handleRequest(request) {
       return new Response("success");
     }
     catch(err) {
-      return new Response(err)
+      return new Response("error",{status:400})
     }
   }
 
@@ -31,13 +31,21 @@ async function handleRequest(request) {
     try {
       let data = await INTERVIEW_KV.list();
 
+      console.log(data.keys);
+
       var pairs = [];
       for (let x=0;x<data.keys.length;x++){
-        let value = await INTERVIEW_KV.get(data.keys[x].name)
-        pairs.push(value)
+        let value = await INTERVIEW_KV.get(data.keys[x].name);
+        let jsonvalue = JSON.parse(value);
+        pairs.push(jsonvalue);
       }
-      console.log(pairs);
-      return new Response(pairs);
+
+      return new Response(JSON.stringify(pairs),{
+        headers:{
+          'Access-Control-Allow-Origin': '*',
+          'Content-type': 'application/json'
+        }
+      });
     }
     catch (err){
       return new Response(err)
